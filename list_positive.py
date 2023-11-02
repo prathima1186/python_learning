7f4b4b6878159cda420a452ca8a694c6660863db
prathima1186 <prathima1186@gmail.com>
2023-11-02 15:12:39 -0400

    Positive numbers program

---
+++ list_positive.py
@@ -0,0 +1,6 @@
+# This program is to print positive numbers in List.
+list1 = [ 2,9,-1,0,-7,5,-10,8,-3,11]
+print("The numbers in the list are:",list1)
+pos_nos = [num for num in list1 if(num >=0)]
+print("The positive numbers in the list are:", pos_nos)
+
